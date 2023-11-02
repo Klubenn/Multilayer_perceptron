aff42ac7c18a5ef036c957239d4bb0d473282cc3
Gita Tristana <gtristan@student.42wolfsburg.de>
2023-11-02 18:49:43 +0100

    started creating model

--- mlp.py
+++ mlp.py
@@ -4,6 +4,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def relu(x: float):
+    if x > 0:
+        return x
+    return 0
+
 class Neuron:
     def __init__(self, weight=None):
         self.weight = weight if weight is not None else random.random()
@@ -13,8 +18,39 @@ class Neuron:
 
 
 class Model:
-    def __init__(self, input_units: int, output_units: int, hidden_units: int, hidden_layers: int):
-        pass
+    def __init__(self,
+                    input_features: int,
+                    output_features: int,
+                    hidden_layers: int,
+                    hidden_features: int,
+                    activation_function,
+                    ):
+        self.input_features = input_features
+        self.output_features = output_features
+        self.hidden_layers = hidden_layers
+        self.hidden_features = hidden_features
+        self.activation_function = activation_function
+
+        self.weights = self._create_layers(initialize=True)
+        self.biases = np.empty(hidden_layers + 1, dtype='float32')
+        self.neurons_outputs = self._create_layers(initialize=False)
+
+    def _create_layers(self, initialize: bool):
+        layers = {}
+        for i in range(self.hidden_layers + 1):
+            # number of matrix rows is equal to the number of input neurons (to the right)
+            rows = self.hidden_features if i != self.hidden_layers else self.output_features
+            # number of matrix columns is equal to the number of output neurons (to the left)
+            columns = self.input_features if i == 0 else self.hidden_features
+            if initialize:
+                layers[i] = np.empty((rows, columns), dtype='float32')
+            else:
+                layers[i] = np.zeros((rows, columns), dtype='float32')
+        return layers
+
+    def calculate_neuron_input(self, layer, neuron_number):
+
+
 
     def forward_pass(self, data):
         pass
